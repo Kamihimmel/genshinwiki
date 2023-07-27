12686baa52cb1f35ee1ff8c65def89a96e34ecab
Shengwu Jiang <shengwu.code@gmail.com>
2023-07-27 16:18:43 +0800

    update for 4.0 v4

--- crawler/all_characters.sh
+++ crawler/all_characters.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/zsh
+#!/bin/bash
 
-for id in $(cat ../lib/characterlist.json | jq -r ".data[].id"); do
+for id in $(cat ../lib/characterlist.json | jq -r ".data[]|select(.spoiler==true)|.id"); do
   echo $id
   python character_ambr.py $id
 done
--- crawler/all_weapons.sh
+++ crawler/all_weapons.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/zsh
+#!/bin/bash
 
-for id in $(cat ../lib/weaponlist.json | jq -r ".data[].id"); do
+for id in $(cat ../lib/weaponlist.json | jq -r ".data[]|select(.spoiler==true)|.id"); do
   echo $id
   python weapon_ambr.py $id
 done
