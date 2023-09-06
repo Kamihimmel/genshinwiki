15e5ef2913e3940085fc111ec7bddcdce3776e77
Shengwu Jiang <shengwu.code@gmail.com>
2023-09-06 14:35:52 +0800

    remove python

--- crawler/all_characters.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for id in $(cat ../lib/characterlist.json | jq -r ".data[]|select(.spoiler==true)|.id"); do
-  echo $id
-  python character_ambr.py $id
-done
--- crawler/all_weapons.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for id in $(cat ../lib/weaponlist.json | jq -r ".data[]|select(.spoiler==true)|.id"); do
-  echo $id
-  python weapon_ambr.py $id
-done
