b23644725697384862d17ac563dbf7189afcb61a
chen.kai <chen.kai@maisiteng.com>
2023-09-23 22:06:35 +0800

    add deploy to oss

---
+++ upload_and_refresh.py
@@ -0,0 +1,38 @@
+import os
+import oss2
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkcdn.request.v20180510 import RefreshObjectCachesRequest
+
+# OSS 配置
+auth = oss2.Auth(os.getenv('OSS_ACCESS_KEY_ID'), os.getenv('OSS_ACCESS_KEY_SECRET'))
+bucket = oss2.Bucket(auth, os.getenv('OSS_ENDPOINT'), os.getenv('OSS_BUCKET'))
+
+# CDN 配置
+client = AcsClient(os.getenv('CDN_ACCESS_KEY_ID'), os.getenv('CDN_ACCESS_KEY_SECRET'), 'cn-beijing')
+
+# 本地文件目录路径
+folder_path = '.'
+
+# 遍历目录并上传文件到 OSS
+for root, dirs, files in os.walk(folder_path):
+    # 忽略隐藏文件夹
+    dirs[:] = [d for d in dirs if not d[0] == '.']
+    
+    for file in files:
+        # 忽略隐藏文件
+        if file.startswith('.'):
+            continue
+            
+        local_file = os.path.join(root, file)
+        oss_file = os.path.relpath(local_file, folder_path).replace('\\', '/')
+        print(f'Uploading {local_file} to {oss_file}')  # 添加调试信息
+        bucket.put_object_from_file(oss_file, local_file)
+
+# 刷新 CDN 缓存
+request = RefreshObjectCachesRequest.RefreshObjectCachesRequest()
+request.set_accept_format('json')
+# request.set_ObjectPath(f"https://{os.getenv('CDN_DOMAIN')}/")
+request.set_ObjectPath('https://genshinwiki.kchlu.com/')
+response = client.do_action_with_exception(request)
+
+print(str(response, encoding='utf-8'))
